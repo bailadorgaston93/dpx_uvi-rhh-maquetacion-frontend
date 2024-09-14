8c8e47c87b1d7640b7102b8d9557ff15ea2c4333
Gastón Bailador <bailadorgaston-it@hotmail.com>
2024-09-14 16:39:12 -0300

    Update FooterApp.js

--- src/components/UI/FooterApp.js
+++ src/components/UI/FooterApp.js
@@ -2,7 +2,7 @@
 export const FooterApp = () => {
     return <div className="footer_app_content">
         <div>
-            <span>©2023 Uvi - Designed and Developed by <a href="https://www.linkedin.com/in/bailadev11/" target="_blank" rel="noreferrer">Gastón E. Bailador</a></span>
+            <span>©2023 Uvi - Designed and Developed by <a href="https://www.linkedin.com/in/devperx/" target="_blank" rel="noreferrer">Gastón E. Bailador</a></span>
         </div>
     </div>;
 }
